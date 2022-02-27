fc392e0d7694261d49f1ddb10e3235121099c77b
Jorge Mario <j-mario9715@hotmail.com>
2022-02-27 17:58:16 -0500

    Punto 3 finalizado

--- Servidor/Core/prueba/src/main/java/com/prueba/farmatodo/services/impl/NumerosServiceImpl.java
+++ Servidor/Core/prueba/src/main/java/com/prueba/farmatodo/services/impl/NumerosServiceImpl.java
@@ -7,25 +7,77 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @Log4j2
 public class NumerosServiceImpl implements INumerosService {
 
 
+    private List<NumeroFelizDto> numeroFeliz;
 
-
-    @SuppressWarnings("static-access")
     @Autowired
-    public NumerosServiceImpl() {
-
+    public NumerosServiceImpl(List<NumeroFelizDto> numeroFeliz) {
+        this.numeroFeliz = numeroFeliz;
     }
 
 
+    public static int[] digitosNumero(int iNumero) {
+
+        String x = Integer.toString(iNumero);
+        int[] iNumeros = new int[x.length()];
+        int iDigito = 0;
+
+        while (iNumero > 0) {
+            iNumeros[iDigito] = iNumero % 10;
+            iNumero = iNumero / 10;
+            iDigito++;
+        }
+        return iNumeros;
+    }
+
     @Override
     public NFelicesDto consultarNumerosFelices(RpaListadoNumerosDto rpaListadoNumerosDto) {
-        return null;
+        numeroFeliz.clear();
+        NFelicesDto nFelicesDto = new NFelicesDto();
+
+        try {
+            for (Integer numero : rpaListadoNumerosDto.getNumeros()) {
+                Set<Integer> iCalculados = new HashSet<Integer>();
+                boolean bRepetido = false;
+                int[] iNumeros = digitosNumero(numero);
+                int iSuma = 0;
+
+                while ((iSuma != 1) && !(bRepetido)) {
+                    iSuma = 0;
+                    for (int x = 0; x < iNumeros.length; x++)
+                        iSuma += Math.pow(iNumeros[x], 2);
+                    iNumeros = digitosNumero(iSuma);
+
+                    if (iCalculados.contains(new Integer(iSuma)))
+                        bRepetido = true;
+                    else
+                        iCalculados.add(new Integer(iSuma));
+                }
+
+                NumeroFelizDto numeroFelizDto = new NumeroFelizDto();
+                numeroFelizDto.setNumber(numero);
+                if (bRepetido) {
+                    numeroFelizDto.setIsHappy(false);
+                } else {
+                    numeroFelizDto.setIsHappy(true);
+                }
+                numeroFeliz.add(numeroFelizDto);
+            }
+            nFelicesDto.setNumbers(numeroFeliz);
+
+        } catch (Exception e) {
+            log.error("Error al consultar los numeros felices", e);
+        }
+
+        return nFelicesDto;
     }
 
     @Override
